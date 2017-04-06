5e11f63834a7728c7885a47b2d388e37886a976a
fanglingfeng <EX-FANGLINGFENG001>
2017-04-06 19:02:22 +0800

    Edittext

---
+++ app/src/main/java/com/example/fanglingfeng/sortname/SearchEditText.java
@@ -0,0 +1,23 @@
+package com.example.fanglingfeng.sortname;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+/**
+ * Created by fanglingfeng on 2017/4/6.
+ */
+
+public class SearchEditText extends android.support.v7.widget.AppCompatEditText {
+    public SearchEditText(Context context) {
+        this(context,null);
+    }
+
+    public SearchEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        
+    }
+}
